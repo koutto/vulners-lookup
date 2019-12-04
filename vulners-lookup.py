8905a2b1cf0a806bb8a5e2b8436c34af40ab6d07
koutto <xst3nz@gmail.com>
2019-12-04 20:43:34 +0100

    Fix bug. Add usage examples. Black Formatting

--- vulners-lookup.py
+++ vulners-lookup.py
@@ -3,8 +3,8 @@
 #
 # vulners-lookup
 # --------------
-# Perform vulnerabilities lookup on Vulners, the largest database (lots of sources including
-# exploit-db, 0day.today, Nessus, OpenVAS...). Ref: https://vulners.com/stats
+# Perform vulnerabilities lookup on Vulners, the largest database (lots of sources
+# including exploit-db, 0day.today, Nessus, OpenVAS...). Ref: https://vulners.com/stats
 #
 # Based on Vulners API (https://github.com/vulnersCom/api)
 #
@@ -23,42 +23,55 @@ import sys
 def colorize(string, color=None, highlight=None, attrs=None):
     """Apply style on a string"""
     # Colors list: https://pypi.org/project/colored/
-    return colored.stylize(string, (colored.fg(color) if color else '') + \
-                                   (colored.bg(highlight) if highlight else '') + \
-                                   (colored.attr(attrs) if attrs else ''))
+    return colored.stylize(
+        string,
+        (colored.fg(color) if color else "")
+        + (colored.bg(highlight) if highlight else "")
+        + (colored.attr(attrs) if attrs else ""),
+    )
+
 
 def remove_non_printable_chars(string):
     """Remove non-ASCII chars like chinese chars"""
-    printable = set("""0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ """)
-    return ''.join(filter(lambda x: x in printable, string))
+    printable = set(
+        """0123456789abcdefghijklmnopqrstuvwxyz"""
+        """ABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ """
+    )
+    return "".join(filter(lambda x: x in printable, string))
+
 
 def table(columns, data, hrules=True):
     """Print a table"""
-    columns = map(lambda x:colorize(x, attrs='bold'), columns)
-    table = prettytable.PrettyTable(hrules=prettytable.ALL if hrules else prettytable.FRAME, field_names=columns)
+    columns = map(lambda x: colorize(x, attrs="bold"), columns)
+    table = prettytable.PrettyTable(
+        hrules=prettytable.ALL if hrules else prettytable.FRAME, field_names=columns
+    )
     for row in data:
         table.add_row(row)
-    table.align = 'l'
+    table.align = "l"
     print(table)
+
 
 def shorten(string, maxlength):
     """Shorten a string if it exceeds a given length"""
     if len(string) <= maxlength:
         return string
     else:
-        return textwrap.wrap(string, maxlength)[0]+'...'
+        return textwrap.wrap(string, maxlength)[0] + "..."
+
 
 def get_cvss_score(vuln, vulners_api):
     """Get CVSS score if available, otherwise get Vulners AI score"""
-    cvss = vuln['cvss']['score']
+    cvss = vuln["cvss"]["score"]
 
     if cvss == 0.0:
         try:
-            return vulners_api.aiScore(vuln['description'])[0]
+            return vulners_api.aiScore(vuln["description"])[0]
         except:
             return 0.0
     else:
         return cvss
+
 
 def color_cvss(cvss):
     """Attribute a color to the CVSS score"""
@@ -68,32 +81,31 @@ def color_cvss(cvss):
         return None
 
     if cvss < 3:
-        color = 'green_3b'
+        color = "green_3b"
     elif cvss <= 5:
-        color = 'yellow_1'
+        color = "yellow_1"
     elif cvss <= 7:
-        color = 'orange_1'
+        color = "orange_1"
     elif cvss <= 8.5:
-        color = 'dark_orange'
+        color = "dark_orange"
     else:
-        color = 'red'
+        color = "red"
     return color
+
 
 def info(string):
     """Print info string"""
-    print('{}{}'.format(colorize('[*] ', color='light_blue', attrs='bold'), string))
+    print("{}{}".format(colorize("[*] ", color="light_blue", attrs="bold"), string))
 
 
 def warning(string):
     """Print warning string"""
-    print('{}{}'.format(colorize('[!] ', color='orange_1', attrs='bold'), string))
+    print("{}{}".format(colorize("[!] ", color="orange_1", attrs="bold"), string))
 
 
 def error(string):
     """Print error string"""
-    print('{}{}'.format(colorize('[!] ', color='red', attrs='bold'), string))
-
-
+    print("{}{}".format(colorize("[!] ", color="red", attrs="bold"), string))
 
 
 def global_search(apikey, query):
@@ -102,80 +114,86 @@ def global_search(apikey, query):
     try:
         vulners_api = vulners.Vulners(api_key=apikey)
     except:
-        error('Unable to connect to Vulners.com. Check internet connection !')
+        error("Unable to connect to Vulners.com. Check internet connection !")
         sys.exit(1)
 
     try:
-        results = vulners_api.search('{}  order:published'.format(query), limit=200)
+        results = vulners_api.search("{}  order:published".format(query), limit=200)
     except Exception as e:
-        error('Unable to get results. Might happen is maximum requests count has been ' \
-            'reached with the API key in use')
+        error(
+            "Unable to get results. Might happen is maximum requests count has been "
+            "reached with the API key in use"
+        )
         sys.exit(1)
 
     nb_results = 0
+    excluded_families = ("info", "blog", "bugbounty", "tools", "advertisement")
+
     for r in results:
-        if r['bulletinFamily'] not in ('info', 'blog', 'bugbounty', 'tools', 'advertisement'):
+        if r["bulletinFamily"] not in excluded_families:
             nb_results += 1
 
     if nb_results == 0:
-        warning('No result has been found !')
+        warning("No result has been found !")
         sys.exit(0)
     else:
-        info('{} results found. Retrieving CVSS scores or computing AI scores if not available...'.format(nb_results))
+        info(
+            "{} results found. Retrieving CVSS scores or computing AI scores if "
+            "not available...".format(nb_results)
+        )
 
-
-    columns = [
-        '#',
-        'Score',
-        'Title',
-        'Description',
-        'URL',
-        'Type',
-    ]
+    columns = ["#", "Score", "Title", "Description", "URL", "Type"]
     data = list()
     i = 1
-    results = []
+    filtered_results = []
     for r in results:
-        if r['bulletinFamily'] not in ('info', 'blog', 'bugbounty', 'tools', 'advertisement'):
+        if r["bulletinFamily"] not in excluded_families:
             score = get_cvss_score(r, vulners_api)
-            type_ = r['bulletinFamily']
-            if type_ == 'exploit':
-                type_ = colorize(type_, color='red', attrs='bold')
+            type_ = r["bulletinFamily"]
 
             result = {
-                'reference': r['id'],
-                'score': score,
-                'title': remove_non_printable_chars(res['title']).replace(';',','),
-                'description': remove_non_printable_chars(res['description']).replace(';',','),
-                'url': res['vhref'],
-                'type': type_,
+                "reference": r["id"],
+                "score": score,
+                "title": remove_non_printable_chars(r["title"]).replace(";", ","),
+                "description": remove_non_printable_chars(r["description"]).replace(
+                    ";", ","
+                ),
+                "url": r["vhref"],
+                "type": type_,
             }
-            results.append(result)
+            filtered_results.append(result)
             i += 1
 
-    return results
+    return filtered_results
 
 
 def software_api(name, version):
-    info('Querying Vulners API for: name="{name}", version={version}...'.format(name=name, version=version))
+    info(
+        'Querying Vulners API for: name="{name}", version={version}...'.format(
+            name=name, version=version
+        )
+    )
 
     try:
-        res = requests.get('https://vulners.com/api/v3/burp/software',
-            params={'software': name, 'version': version, 'type': 'software'})
+        res = requests.get(
+            "https://vulners.com/api/v3/burp/software",
+            params={"software": name, "version": version, "type": "software"},
+        )
     except:
-        error('Unable to query the Vulners API. Check Internet Connection !')
+        error("Unable to query the Vulners API. Check Internet Connection !")
         sys.exit(1)
 
     if not res:
-        error('Empty result returned by Vulners API')
+        error("Empty result returned by Vulners API")
         sys.exit(1)
 
     try:
         json = res.json()
     except:
-        error('Response from Vulners endpoint is not valid JSON data. Cannot process it !')
+        error(
+            "Response from Vulners endpoint is not valid JSON data. Cannot process it !"
+        )
         sys.exit(1)
-
 
     # Empty result :
     # {
@@ -186,117 +204,127 @@ def software_api(name, version):
     #   }
     # }
     results = []
-    if json['result'] == 'OK':
-        for res in json['data']['search']:
+    if json["result"] == "OK":
+        for res in json["data"]["search"]:
             result = {
-                'reference': res['_id'],
-                'score': res['_source']['cvss']['score'] if 'cvss' in res['_source'] \
-                    and 'score' in res['_source']['cvss'] else None,
-                'title': remove_non_printable_chars(res['_source']['title']).replace(';',','),
-                'description': remove_non_printable_chars(res['_source']['description']).replace(';',','),
-                'url': res['_source']['href'],
-                'type': res['_source']['type']
+                "reference": res["_id"],
+                "score": res["_source"]["cvss"]["score"]
+                if "cvss" in res["_source"] and "score" in res["_source"]["cvss"]
+                else None,
+                "title": remove_non_printable_chars(res["_source"]["title"]).replace(
+                    ";", ","
+                ),
+                "description": remove_non_printable_chars(
+                    res["_source"]["description"]
+                ).replace(";", ","),
+                "url": res["_source"]["href"],
+                "type": res["_source"]["type"],
             }
-            if result['score'] == 0.0:
-                result['score'] = None
+            if result["score"] == 0.0:
+                result["score"] = None
 
             results.append(result)
 
     else:
-        warning('Vulners API returns 0 result')
+        warning("Vulners API returns 0 result")
         sys.exit(0)
 
     return results
 
 
-
 def display_results(results):
-    columns = [
-        '#',
-        'Score',
-        'Title',
-        'Description',
-        'URL',
-        'Type',
-    ]
+    columns = ["#", "Score", "Title", "Description", "URL", "Type"]
     data = list()
     for r in results:
-        data.append([
-            r['reference'],
-            colorize(r['score'], color=color_cvss(r['score']), attrs='bold') if r['score'] else '',
-            textwrap.fill(remove_non_printable_chars(r['title']), 30),
-            textwrap.fill(remove_non_printable_chars(r['description']), 50),
-            textwrap.fill(r['url'],78),
-            r['type'],
-        ])
+        data.append(
+            [
+                textwrap.fill(r["reference"], 20),
+                colorize(r["score"], color=color_cvss(r["score"]), attrs="bold")
+                if r["score"]
+                else "",
+                textwrap.fill(remove_non_printable_chars(r["title"]), 30),
+                textwrap.fill(remove_non_printable_chars(r["description"]), 50),
+                textwrap.fill(r["url"], 78),
+                colorize(r["type"], color="red", attrs="bold")
+                if r["type"] == "exploit"
+                else r["type"],
+            ]
+        )
 
-    #pprint.pprint(results)
+    # pprint.pprint(results)
 
-    info('{} results returned:'.format(len(results)))
+    info("{} results returned:".format(len(results)))
     table(columns, data, hrules=True)
-
-
-
 
 
 # Command-line parsing
 # -----------------------------------------------------------------------------
-# USAGE = """
-
 # Examples:
-
 # * Mode "all" (requires API key):
 #     python3 vulners-lookup.py all --apikey <API-key> "Apache Tomcat 8.5.0"
-#     python3 vulners-lookup.py --apikey <API-key> 'affectedSoftware.name:"Microsoft IIS" AND affectedSoftware.version:"6.0"'
-
+#     python3 vulners-lookup.py --apikey <API-key> 'affectedSoftware.name:"Microsoft IIS"
+#         AND affectedSoftware.version:"6.0"'
 # * Mode "software":
 # """
 
-parser = argparse.ArgumentParser()
+DESCRIPTION = """
+Usage Examples:
+---------------
+- Global search (requires API key):
+    python3 vulners-lookup.py all --apikey <API-key> 'Apache Tomcat 8.5.0'
+    python3 vulners-lookup.py all --apikey <API-key> 'affectedSoftware.name:"Microsoft IIS" AND affectedSoftware.version:"6.0"'
+
+- Software search (API used by Nmap vulners NSE script & Burp add-on):
+    python3 vulners-lookup.py software --name proftpd --version 1.3
+"""
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter, epilog=DESCRIPTION
+)
 
 parser.add_argument(
-    '--csv',
-    help='Output as CSV file (fields separated by semicolon)',
-    action='store',
-    metavar='<output-filename>',
-    dest='csv'
+    "--csv",
+    help="Output as CSV file (fields separated by semicolon)",
+    action="store",
+    metavar="<output-filename>",
+    dest="csv",
 )
 parser.add_argument(
-    '--display-csv',
-    help='Display CSV data at the end',
-    action='store_true',
-    dest='displaycsv'
+    "--display-csv",
+    help="Display CSV data at the end",
+    action="store_true",
+    dest="displaycsv",
 )
 
-subparsers = parser.add_subparsers(dest='mode')
+subparsers = parser.add_subparsers(dest="mode")
 
-parser_all = subparsers.add_parser('all')
+parser_all = subparsers.add_parser("all")
 parser_all.add_argument(
-    '--apikey', 
-    help     = 'Vulners API key', 
-    action   = 'store', 
-    dest     = 'apikey', 
-    metavar  = '<API-key>', 
-    required = True,
-    default  = None
+    "--apikey",
+    help="Vulners API key",
+    action="store",
+    dest="apikey",
+    metavar="<API-key>",
+    required=True,
+    default=None,
 )
-parser_all.add_argument('query', help='Query', action='store')
+parser_all.add_argument("query", help="Query", action="store")
 
-parser_software = subparsers.add_parser('software')
+parser_software = subparsers.add_parser("software")
 parser_software.add_argument(
-    '--name',
-    help='Product name',
-    action='store',
-    dest='name',
-    metavar='<name>',
-    required=True
+    "--name",
+    help="Product name",
+    action="store",
+    dest="name",
+    metavar="<name>",
+    required=True,
 )
 parser_software.add_argument(
-    '--version',
-    help='Version number',
-    action='store',
-    dest='version',
-    metavar='<version>',
+    "--version",
+    help="Version number",
+    action="store",
+    dest="version",
+    metavar="<version>",
     required=True,
 )
 
@@ -305,37 +333,38 @@ args = parser.parse_args()
 
 # Processing
 # -----------------------------------------------------------------------------
-if args.mode == 'all':
+if args.mode == "all":
     results = global_search(args.apikey, args.query)
-elif args.mode == 'software':
+elif args.mode == "software":
     results = software_api(args.name, args.version)
 else:
     sys.exit(1)
 
 display_results(results)
 
-#pprint.pprint(results)
+# pprint.pprint(results)
 if args.csv or args.displaycsv:
-    csvdata = 'ID;CVSS;Title;Description;URL;Type\n'
+    csvdata = "ID;CVSS;Title;Description;URL;Type\n"
     for r in results:
-        csvdata += '{ref};{cvss};{title};{description};{url};{type}\n'.format(
-            ref=r['reference'],
-            cvss=r['score'],
-            title=r['title'],
-            description=r['description'],
-            url=r['url'],
-            type=r['type']) 
+        csvdata += "{ref};{cvss};{title};{description};{url};{type}\n".format(
+            ref=r["reference"],
+            cvss=r["score"],
+            title=r["title"],
+            description=r["description"],
+            url=r["url"],
+            type=r["type"],
+        )
 
     if args.displaycsv and len(results) > 0:
         print()
-        info('CSV output:')
+        info("CSV output:")
         print(csvdata)
 
-    if args.csv: 
+    if args.csv:
         try:
-            with open(args.csv, 'w') as f:
+            with open(args.csv, "w") as f:
                 f.write(csvdata)
 
-            info('CSV output written to {csv} file'.format(csv=args.csv))
+            info("CSV output written to {csv} file".format(csv=args.csv))
         except Exception as e:
-            error('An error occured when trying to write CSV output: {exc}'.format(exc=e))
+            error("An error occured when trying to write CSV output: {exc}".format(exc=e))
